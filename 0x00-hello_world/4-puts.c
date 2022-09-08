eeb40d427ad675d46ed6f88f90ed2a5f29326712
ashimolowo <tobayonline@gmail.com>
2022-09-08 13:44:49 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,23 @@
-#!/bin/bash
-"\"Programming is like building a multilingual puzzle"
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
