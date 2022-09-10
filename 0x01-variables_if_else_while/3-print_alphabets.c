12a416931ae1e27f406348cac6afa2bcd1c52ce9
ashimolowo <tobayonline@gmail.com>
2022-09-10 15:46:40 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints a-z to std output
+ * Return: Exit status is zero if successfull
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+	for (i = 65; i <= 90; i++)
+	}
+		putchar(i);
+	} putchar('\n');
+	return (0);
+}
