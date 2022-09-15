599f0f0081eb3d3e262be8f81759eab0318a66c8
ashimolowo <tobayonline@gmail.com>
2022-09-15 07:50:12 -0700

    nested loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,7 +2,7 @@
 #include <ctype.h>
 
 /**
- * _isupper - Checks for uppercase ASCII characters
+ * 0_isupper - Checks for uppercase ASCII characters
  * @c: This is an ASCII character
  *
  * Return: 0 if not uppercase, 1 if uppercase
