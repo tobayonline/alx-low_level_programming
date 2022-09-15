ea09ad155be14265473a48003750338dceaf6017
ashimolowo <tobayonline@gmail.com>
2022-09-15 10:49:43 -0700

    nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - Prints '\' n times
- * @n: specifies how many times \ is printed
- *
- * Return: Null
+ * print_diagonal - Draws a diagonal line using the \ character.
+ * @n: The number of \ characters to be printed.
  */
 void print_diagonal(int n)
 {
