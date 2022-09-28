fc793e2165b8880463581a7fb42aab5b059e3f39
ashimolowo <tobayonline@gmail.com>
2022-09-28 06:49:02 -0700

    Recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -36,28 +36,15 @@ int p1(char *s, int l)
 	return (0);
 }
 
-
-
 /**
-
  * is_palindrome - palindrome
-
  * @s: pointer to string
-
  * Return: recursion
-
  */
 
-
-
 int is_palindrome(char *s)
-
 {
-
 	int len = _strlen_recursion(s);
 
-
-
 	return (p1(s, len - 1));
-
 }
